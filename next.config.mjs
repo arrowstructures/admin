b2f23c3a54dc49ec2f46efd2058e6160837e7304
arrowstructures <arrowstructures@gmail.com>
2025-06-06 19:25:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'out',
-  experimental: {
-    appDir: true,
-  },
+  }
 }
 
 export default nextConfig
