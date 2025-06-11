dc9b134dd914faec5e4ef8acc484cf27f3bfa738
arrowstructures <arrowstructures@gmail.com>
2025-06-11 15:41:30 +0530

    Add files via upload

---
+++ app/clients/edit/[id]/generate-static-params.ts
@@ -0,0 +1,5 @@
+// This file is required for static export but we'll return empty array
+// since we're not using static export anymore
+export async function generateStaticParams() {
+  return []
+}
